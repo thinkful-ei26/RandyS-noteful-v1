2e97d1f9607dc521b08f8af39b4b893d965a7bec
randy sartor <randal.sartor@gmail.com>
2018-11-12 14:32:17 -0500

    added GET request  for all items

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
