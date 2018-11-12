a42364d3b171bc1f90985423757aa966de7d02a4
randy sartor <randal.sartor@gmail.com>
2018-11-12 14:45:39 -0500

    added single note  GET

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = data.find(note => note.id === Number(id));
+  res.json(note);
+});
+
+
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
