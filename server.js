404f3cdbc7baa23cbeb52098b2f6cf8d16246c22
randy sartor <randal.sartor@gmail.com>
2018-11-13 15:32:24 -0500

    logging and errors

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
 
+const { PORT } = require('./config');
+
 const express = require('express');
 
 const app = express();
 
+const logger = require('./middleware/logger');
+
+
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
+app.use(logger);
 
 app.get('/api/notes', (req, res) => {
   const searchTerm = req.query.searchTerm;
@@ -30,9 +36,25 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(note);
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
 
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
 
-app.listen(8080, function () {
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
