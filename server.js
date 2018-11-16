e150df1268aca890cb799edeba554fee19849c68
Randy Sartor <randal.sartor@gmail.com>
2018-11-16 17:40:49 -0500

    Revert "added all tests"

--- server.js
+++ server.js
@@ -51,12 +51,8 @@ app.use(function (err, req, res, next) {
 });
 
 // Listen for incoming connections
-if (require.main === module) {
-  app.listen(PORT, function () {
-    console.info(`Server listening on ${this.address().port}`);
-  }).on('error', err => {
-    console.error(err);
-  });
-}
-
-module.exports = app;
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
